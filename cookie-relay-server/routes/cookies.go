fd43fedef52434defbcc2f6e79a5c55d1164e3c7
7x11x13 <x7x11x13@gmail.com>
2025-01-06 14:40:40 -0500

    Store cookies with redis JSON, add new route

--- cookie-relay-server/routes/cookies.go
+++ cookie-relay-server/routes/cookies.go
@@ -16,31 +16,37 @@ type CookiePartitionKey struct {
 }
 
 type Cookie struct {
-	Domain           string `json:"domain"`
-	ExpirationDate   int64  `json:"expirationDate"`
-	FirstPartyDomain string `json:"firstPartyDomain,omitempty"`
-	HostOnly         bool   `json:"hostOnly"`
-	HttpOnly         bool   `json:"httpOnly"`
-	Name             string `json:"name"`
+	Domain           string             `json:"domain"`
+	ExpirationDate   int64              `json:"expirationDate"`
+	FirstPartyDomain string             `json:"firstPartyDomain,omitempty"`
+	HostOnly         bool               `json:"hostOnly"`
+	HttpOnly         bool               `json:"httpOnly"`
+	Name             string             `json:"name"`
 	PartitionKey     CookiePartitionKey `json:"partitionKey,omitempty"`
-	Path             string `json:"path"`
-	Secure           bool   `json:"secure"`
-	Session          bool   `json:"session"`
-	SameSite         string `json:"sameSite"`
-	StoreId          string `json:"storeId"`
-	Value            string `json:"value"`
+	Path             string             `json:"path"`
+	Secure           bool               `json:"secure"`
+	Session          bool               `json:"session"`
+	SameSite         string             `json:"sameSite"`
+	StoreId          string             `json:"storeId"`
+	Value            string             `json:"value"`
 }
 
 func InitRoutes(r *gin.Engine) {
 	r.GET("/cookies/:website/:userId", GetCookies)
+	r.GET("/cookies/:website/:userId/:cookieName", GetCookie)
 	r.POST("/cookies/:website/:userId", SetCookies)
 }
 
-func GetCookies(ctx *gin.Context) {
+func GetCookie(ctx *gin.Context) {
 	website := ctx.Param("website")
 	userId := ctx.Param("userId")
+	cookieName := ctx.Param("cookieName")
 	key := fmt.Sprintf("cookies:%s:%s", website, userId)
-	json_str, err := db.Redis.Get(ctx, key).Result()
+	if db.Redis.Exists(ctx, key).Val() == 0 {
+		ctx.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	json_str, err := db.Redis.JSONGet(ctx, key, cookieName).Result()
 	if err == redis.Nil {
 		ctx.AbortWithStatus(http.StatusNotFound)
 		return
@@ -48,13 +54,43 @@ func GetCookies(ctx *gin.Context) {
 		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	var cookies []Cookie
+	var cookie Cookie
+	err = json.Unmarshal([]byte(json_str), &cookie)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, cookie)
+}
+
+func GetCookies(ctx *gin.Context) {
+	website := ctx.Param("website")
+	userId := ctx.Param("userId")
+	key := fmt.Sprintf("cookies:%s:%s", website, userId)
+	if db.Redis.Exists(ctx, key).Val() == 0 {
+		ctx.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	json_str, err := db.Redis.JSONGet(ctx, key).Result()
+	if err == redis.Nil {
+		ctx.AbortWithStatus(http.StatusNotFound)
+		return
+	} else if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	var cookies map[string]Cookie
 	err = json.Unmarshal([]byte(json_str), &cookies)
 	if err != nil {
 		ctx.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	ctx.JSON(http.StatusOK, cookies)
+
+	arr := []Cookie{}
+	for _, cookie := range cookies {
+		arr = append(arr, cookie)
+	}
+	ctx.JSON(http.StatusOK, arr)
 }
 
 func cookiesFromJSON(jsonData []byte) ([]Cookie, error) {
@@ -74,13 +110,21 @@ func SetCookies(ctx *gin.Context) {
 		ctx.AbortWithError(400, err)
 		return
 	}
-	json, err := json.Marshal(cookies)
-	if err != nil {
-		ctx.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
 	website := ctx.Param("website")
 	userId := ctx.Param("userId")
 	key := fmt.Sprintf("cookies:%s:%s", website, userId)
-	db.Redis.Set(ctx, key, string(json), 0).Result()
+	if db.Redis.Exists(ctx, key).Val() == 0 {
+		_, err := db.Redis.JSONSet(ctx, key, "$", "{}").Result()
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+	}
+	for _, cookie := range cookies {
+		_, err = db.Redis.JSONSet(ctx, key, cookie.Name, cookie).Result()
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+	}
 }
